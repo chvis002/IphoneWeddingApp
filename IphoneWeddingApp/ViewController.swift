5358a93d9c27954920241dd3f804e8c12401206e
Christopher <chvis002@Christophers-Mac.local>
2015-06-02 04:10:13 -0700

    I managed to make some functionality :)

--- IphoneWeddingApp/ViewController.swift
+++ IphoneWeddingApp/ViewController.swift
@@ -10,7 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var randomLabel: UILabel!
+    
+    @IBOutlet weak var showButton: UIButton!
+    @IBOutlet weak var hideButton: UIButton!
+    
+    
+    
+    @IBAction func showAction(sender: UIButton) {
+        
+        randomLabel.hidden=false
+        
+    }
+    
+    
+    
+    @IBAction func hideActuib(sender: UIButton) {
+        
+        randomLabel.hidden = true
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
+        
+        randomLabel.hidden = true
+        
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
